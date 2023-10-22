f5d06d4320aa224506cda8dd014c010d8d1fd02e
Fernando Oliveira <nandooliveira.al@gmail.com>
2023-10-22 19:34:19 -0300

    Store session

--- api/events.py
+++ api/events.py
@@ -5,6 +5,7 @@ from flask_restful import Resource, reqparse
 from atena.repositories.events_repository import EventsRepository
 
 parser = reqparse.RequestParser()
+parser.add_argument('currentSession', type=str, help='Session Name')
 parser.add_argument('type', type=str, help='Type of Event')
 parser.add_argument('createdAt', type=int, help='Created Timestamp')
 parser.add_argument('data', type=dict, help='Data of Event')
@@ -23,6 +24,7 @@ class Events(Resource):
 
     def post(self):
         args = parser.parse_args()
+        print(args)
         self.repository.create(args)
 
         return {"status": "Success", "message": "Event successfully created"}
